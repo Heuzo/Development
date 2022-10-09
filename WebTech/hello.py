f99cd3785e074fc8da831b2c0cd4f55fea0296ae
heuzo <vlad.neight21@gmail.com>
2022-10-09 13:06:03 +0300

    hello py modified

--- WebTech/hello.py
+++ WebTech/hello.py
@@ -1,1 +1,8 @@
-print('hello py')
+entered_list = input("Введите список чисел, разделенных пробелом: ").split()
+list1 = [int(i) for i in entered_list]
+list2 = []
+
+for i in list1:
+    if list1.count(i) == 1:
+        list2.append(i)
+print(list2)
